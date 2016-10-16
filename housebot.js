fecc26e4d8f810e161d504210bef15b8888a8a91
CoolMan119 <littlelettere@gmail.com>
2016-10-16 12:39:03 -0400

    Added a few commands and added comments
    + New Command: ban
    + Added Comments

--- housebot.js
+++ housebot.js
@@ -1,21 +1,23 @@
 const Discord = require("discord.js");
-const yt = require("ytdl-core")
+const yt = require("ytdl-core");
 const bot = new Discord.Client();
-const prefix = '>>';
-const token = 'Token Here';
+const now = require("performance-now"); 
+const prefix = '>>'; // Prefix for commands, you can change this
+const token = 'Token goes here'; // Token to login to bot goes here
+
 
 // HouseBot by houseofkraft
 // Please don't copy this
 
 
-bot.on("ready", () => {
+bot.on("ready", () => { // Executes the code below when the bot is ready for use
 	bot.user.setStatus("online", "HouseBot | >>help");
 	console.log("Ready!")
 });
 
 
    
-bot.on("message", message => { 
+bot.on("message", message => {  // Executes all the code below when a user sends a message
 function checkPerm(member, perm) { // This function will come in handy
     if( message.member.roles.filter(r=>r.hasPermission(perm)).size > 0) {
         return true
@@ -23,31 +25,21 @@ function checkPerm(member, perm) { // This function will come in handy
     else {
         return false
     }
-};	
+}	
 
-function play(url) {
-	    const voiceChannel = message.member.voiceChannel;
-	    let stream = yt(url, {audioonly: true});
-        const dispatcher = voiceChannel.playStream(stream);
-		message.channel.sendMessage("Now playing **" + url + "**");
-        dispatcher.on('end', () => {
-		  message.channel.sendMessage("YouTube Video is over.");
-		});
+function play(url) { // A little broken at the moment
+// play streams using ytdl-core
+const ytdl = require('ytdl-core');
+const streamOptions = { seek: 0, volume: 1 };
+   const stream = ytdl(url, {filter : 'audioonly'});
+   const dispatcher = connection.playStream(stream, streamOptions);
 }
 
 
 
 	
  
-
-if (message.content.startsWith(prefix + "ping")) {
-    var startTime = now();
-    msg.channel.sendMessage("Pinging...")
-        .then(message => {
-            var endTime = now();
-            return message.edit("Ping took ${(endTime - startTime).toFixed(3)} ms.");
-        }).catch(console.error);
-}
+// Commands
 if (message.content.startsWith(prefix + "about"))  message.channel.sendMessage("HouseBot Beta made by houseofkraft");  
 if (message.content.startsWith(prefix + "help"))  message.channel.sendMessage("```Commands:\n help - Gets help for commands.\n ping - Pong!\n about - Show's who made this bot.\n logo - HouseBot logo\n winlogo - Windows logo\n invite - Tell's you the link to invite HouseBot to other Discord Servers\n isadmin - Says if your an admin (admin perm) or not\n say <message> - Says a message\n kick <person> - Kick's the person (Admins Only) \n music_join - Makes the bot join the voice channel \nBy houseofkraft```");  
 if (message.content.startsWith(prefix + "logo"))  message.channel.sendFile("http://i.imgur.com/yy4aG6o.png");
@@ -61,10 +53,10 @@ if (message.content.startsWith(prefix + "say")) {
     let saytext = message.content.split(" ").slice(1);
     message.channel.sendMessage(saytext)
 }
-if (message.content.startsWith(prefix + "kick")) {
+if (message.content.startsWith(prefix + "kick")) { // Admin Command
      let userToKick = message.mentions.users.first();
      if (checkPerm(message.author, "KICK_MEMBERS") === true) {
-      if (checkPerm(userToKick, "ADMINISTRATOR") === false) {
+      if (checkPerm(userToKick, "ADMINISTRATOR") === true) {
           message.channel.sendMessage("You cannot kick admins!");
       }
       else {
@@ -73,6 +65,27 @@ if (message.content.startsWith(prefix + "kick")) {
       }
   };
 }
+
+if (message.content.startsWith(prefix + "ban")) { // Admin Command
+     let userToKick = message.mentions.users.first();
+     if (checkPerm(message.author, "KICK_MEMBERS") === true) {
+      if (checkPerm(userToKick, "ADMINISTRATOR") === true) {
+          message.channel.sendMessage("You cannot ban admins!");
+      }
+      else {
+               message.guild.member(userToKick).ban();
+             message.channel.sendMessage("Banned " + userToKick + " by " + message.author);
+      }
+  };
+}
+
+if (message.content.startsWith(prefix + "ping")) {
+    var old = message.createdTimestamp();
+  message.channel.sendMessage("Ping?")
+  var n = message.createdTimestamp();
+  message.edit("Pong! (took " + n - old + " ms");
+}
+
 if (message.content.startsWith(prefix + "music_join")) {
   const voiceChannel = message.member.voiceChannel;
   if (!voiceChannel || voiceChannel.type !== 'voice') {
@@ -93,7 +106,7 @@ if (message.content.startsWith(prefix + "music_leave")) {
   message.channel.sendMessage(":white_check_mark: Sucessfully left " + voiceChannel);
 }
 
-if (message.content.startsWith(prefix + "play")) {
+if (message.content.startsWith(prefix + "play")) { // Don't get this confused with the play function, this just activates the play function
 	let args = message.content.split(" ").slice(1);
 	let song = args[0];
 	play(song);
